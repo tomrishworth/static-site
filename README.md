017665ce614da703446865d1b5a08e51dd9a72a5
Greg Shaw <greg@webscope.co.nz>
2016-03-22 16:04:55 +1300

    Added handlebars.

--- README.md
+++ README.md
@@ -5,3 +5,16 @@ Inculdes Bootstrap 4
 
 `npm install` to get setup
 `gulp` to compile scss and watch with livereload
+
+
+# {{#extend "parent-template-name"}}
+Extend pulls in a parent template.
+
+# {{#block "block-name"}}
+Block allows you to define sections that the child templates can insert content into.
+
+# {{#content "block-name"}}
+Content is injected into blocks with a matching name.
+
+# {{ > component-name}}
+A component whose file basename
--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,51 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
 var livereload = require('gulp-livereload');
 var autoprefixer = require('gulp-autoprefixer');
 var minifyCss = require('gulp-minify-css');
+var handlebars = require('gulp-compile-handlebars');
+var layouts = require('handlebars-layouts');
+//
+handlebars.Handlebars.registerHelper(layouts(handlebars.Handlebars));
 
 var jsPaths = [
     "bower_components/jquery/dist/jquery.js",
     "bower_components/bootstrap/dist/js/bootstrap.js",
     "src/js/app.js",
 ];
+
+var handlebarsConfig = {
+    index: {
+        templateData: {
+            dummyData: [
+                {
+                    name: 'Michael'
+                },
+                {
+                    name: 'Alex'
+                },
+                {
+                    name: 'Simon'
+                }
+            ]
+        },
+        options: {
+            ignorePartials: true, //ignores the unknown footer2 partial in the handlebars template, defaults to false
+            batch : [
+                './src/handlebars/',
+                './src/handlebars/components/'
+            ],
+            helpers : {
+                capitals : function(str){
+                    return str.toUpperCase();
+                }
+            }
+        }
+    }
+};
 
 gulp.task('sass', function () {
   gulp.src('src/sass/**/*.scss')
@@ -24,7 +59,7 @@ gulp.task('sass', function () {
     }))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist/css'))
-    .pipe(livereload({ start: true }))
+    //.pipe(livereload({ start: true }))
 });
 
 gulp.task('scripts', function() {
@@ -35,10 +70,21 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('dist/js'));
 });
 
+gulp.task('handlebars', function() {
+    return gulp.src('src/handlebars/views/*.handlebars')
+        .pipe(handlebars(handlebarsConfig.index.templateData, handlebarsConfig.index.options))
+        .pipe(rename(function (path) {
+            path.dirname += '/' + path.basename;
+            path.extname = '.html';
+        }))
+        .pipe(gulp.dest('./dist/views'));
+});
+
 gulp.task('watch', function() {
   livereload.listen();
   gulp.watch('src/sass/**/*.scss', ['sass']);
   gulp.watch('src/js/**/*.js', ['scripts']);
+  gulp.watch('src/handlebars/**/*.handlebars', ['handlebars']);
 });
 
-gulp.task('default', ['sass', 'scripts']);
+gulp.task('default', ['sass', 'scripts', 'handlebars']);
